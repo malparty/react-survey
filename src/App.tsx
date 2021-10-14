855d4e1ffcd5f46d7070203913007735688ac249
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-14 16:56:48 +0700

    [#5] Add background-image component for base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './assets/stylesheets/application.scss';
+import BackgroundImage from './components/BackgroundImage';
 import LoginScreen from './screens/Login';
 
 export default class App extends Component {
   render() {
     return (
       <div className="application">
+        <BackgroundImage />
         <LoginScreen />
       </div>
     );
