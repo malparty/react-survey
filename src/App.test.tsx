1edde06c08a3990e5d8efa9509dedddae276f33a
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-18 16:57:03 +0700

    [#6] Remove enzyme packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { render, screen } from '@testing-library/react';
-=======
-import { shallow } from 'enzyme';
-
->>>>>>> c3b396d ([#6] Add test for LoginScreen component and improve test config)
-=======
 import { render } from '@testing-library/react';
->>>>>>> 87aac3e ([#6] Use react-testing-lib instead of enzyme for testing App and LoginScreen components)
 import App from './App';
 
 describe('<App />', () => {
