22235267d71f36d263093b635a8e993e57b49cdc
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-19 18:08:30 +0700

    [#10] Create base structure for login form

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,23 @@
+import { PureComponent } from 'react';
+
+export default class LoginForm extends PureComponent {
+  render() {
+    return (
+      <div className="login-form">
+        <form>
+          <div className="login-form__field">
+            <label className="login-form__label">Email</label>
+            <input className="login-form__input" type="email" />
+          </div>
+          <div className="login-form__field">
+            <label className="login-form__label">Password</label>
+            <input className="login-form__input" type="password" />
+          </div>
+          <div className="login-form__field">
+            <button className="btn btn-light" type="submit">Sign in</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
