08985c24fa3e0ca445091be61376aaae4f5df55d
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-20 10:04:20 +0700

    [#5] Add background gradiant and gray-900 value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import './assets/stylesheets/application.scss';
 import BackgroundImage from './components/BackgroundImage';
 import LoginScreen from './screens/Login';
