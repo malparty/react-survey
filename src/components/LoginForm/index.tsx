e62f1a6c5dee9dd5f7b0d417579be082c8cd0f8c
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-20 11:54:06 +0700

    [#10] Add style for login form

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -7,11 +7,11 @@ export default class LoginForm extends PureComponent {
         <form>
           <div className="login-form__field">
             <label className="login-form__label">Email</label>
-            <input className="login-form__input" type="email" />
+            <input className="login-form__input form-control" type="email" />
           </div>
           <div className="login-form__field">
             <label className="login-form__label">Password</label>
-            <input className="login-form__input" type="password" />
+            <input className="login-form__input form-control" type="password" />
           </div>
           <div className="login-form__field">
             <button className="btn btn-light" type="submit">Sign in</button>
