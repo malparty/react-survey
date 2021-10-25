cc8bb0da8693bd4c5fc38d913e926d94261c2c96
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-25 15:49:24 +0700

    [#38] Add i18n packages for react

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,8 +1,8 @@
-import { PureComponent } from 'react';
 import nimbleLogo from '@assets/images/nimble-logo.png';
 import { Formik, Field, Form, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 import LoginFormErrors from './Errors';
+import { useTranslation } from 'react-i18next';
 
 export interface LoginFormValues {
   email: string;
@@ -17,51 +17,53 @@ const LoginSchema = Yup.object().shape({
     .required('Please fill in your password.')
 });
 
-export default class LoginForm extends PureComponent {
-  async submitLoginForm(values: LoginFormValues, { setSubmitting }: FormikHelpers<LoginFormValues>) {
+const LoginForm = () => {
+  const { t } = useTranslation();
+
+  const submitLoginForm = async (values: LoginFormValues, { setSubmitting }: FormikHelpers<LoginFormValues>) => {
     await new Promise((resolve) => setTimeout(resolve, 2000)); // Will be replaced by API call in BackEnd Task
 
     console.log(JSON.stringify(values, null, 2)); // Will be replaced by authentication & redirection in BackEnd Task
     setSubmitting(false);
-  }
+  };
 
-  render() {
-    return (
-      <div className="login-form">
-        <img className="login-form__header-image" src={nimbleLogo} alt="Nimble Logo" />
-        <div className="login-form__header-title">Sign in to Nimble</div>
-        <Formik
-          initialValues={{
-            email: '',
-            password: ''
-          }}
-          onSubmit={this.submitLoginForm}
-          validationSchema={LoginSchema}
-        >
-          {(formData) => (
-            <Form>
-              <LoginFormErrors {...formData} />
-              <div className="login-form__field">
-                <label className="login-form__label" htmlFor="email">
-                  Email
-                </label>
-                <Field className="login-form__input form-control" type="email" name="email" id="email" />
-              </div>
-              <div className="login-form__field">
-                <label className="login-form__label" htmlFor="password">
-                  Password
-                </label>
-                <Field className="login-form__input form-control" type="password" name="password" id="password" />
-              </div>
-              <div className="login-form__field">
-                <button className="btn btn-light" type="submit">
-                  Sign in
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="login-form">
+      <img className="login-form__header-image" src={nimbleLogo} alt="Nimble Logo" />
+      <div className="login-form__header-title">{t('login.title')}</div>
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        onSubmit={submitLoginForm}
+        validationSchema={LoginSchema}
+      >
+        {(formData) => (
+          <Form>
+            <LoginFormErrors {...formData} />
+            <div className="login-form__field">
+              <label className="login-form__label" htmlFor="email">
+                Email
+              </label>
+              <Field className="login-form__input form-control" type="email" name="email" id="email" />
+            </div>
+            <div className="login-form__field">
+              <label className="login-form__label" htmlFor="password">
+                Password
+              </label>
+              <Field className="login-form__input form-control" type="password" name="password" id="password" />
+            </div>
+            <div className="login-form__field">
+              <button className="btn btn-light" type="submit">
+                Sign in
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
