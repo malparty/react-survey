78eb180dc931e6578585bd721a10341f48f5e21a
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-25 10:45:25 +0700

    [#7] Use findByDataId commands instead of get in cypress test

--- cypress/integration/UserAuthentication/login.spec.ts
+++ cypress/integration/UserAuthentication/login.spec.ts
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
 
 describe('When an unauthenticated user connects to the app', () => {
-  const LOGIN_SCREEN_CONTAINER_SELECTOR = '[data-test-id=login-screen-container]';
+  const LOGIN_SCREEN_CONTAINER_TEST_ID = 'login-screen-container';
 
   it('Successfully loads', () => {
     cy.visit('/');
@@ -10,6 +11,6 @@ describe('When an unauthenticated user connects to the app', () => {
   it('Displays a "Sign in to Nimble" message', () => {
     cy.visit('/');
 
-    expect(cy.get(LOGIN_SCREEN_CONTAINER_SELECTOR).contains('Sign in to Nimble'));
+    expect(cy.findByTestId(LOGIN_SCREEN_CONTAINER_TEST_ID).contains('Sign in to Nimble'));
   });
 });
