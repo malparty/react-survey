18928f00709349e9086671a8a54e802d275a821d
Xavier MALPARTY <xavier@nimblehq.co>
2021-10-26 18:09:16 +0700

    [#38] Add UI tests for the LanguageSwitch component

---
+++ cypress/integration/LanguageSwitch/language_switch.spec.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+/// <reference types="@testing-library/cypress" />
+
+describe('When a user switch the current language from English to French', () => {
+  it('replaces French by Français', () => {
+    cy.visit('/');
+
+    const frenchLink = cy.findByText('French').click();
+
+    expect(frenchLink.contains('Français'));
+  })
+})
+
+describe('When a user switch back to English after switching to French', () => {
+  it('replaces Anglais back to English', () => {
+    cy.visit('/');
+
+    cy.findByText('French').click();
+    const englishLink = cy.findByText('Anglais').click();
+
+    expect(englishLink.contains('English'));
+  })
+})
+
